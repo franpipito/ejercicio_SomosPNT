d4a35da8d46da43d576143076b82a7af0dba3ff8
franpipito <franpipito7@gmail.com>
2025-07-18 16:03:03 -0300

    Clase producto creada

---
+++ src/com/supermercado/pnt/Main.java
@@ -0,0 +1,27 @@
+package com.supermercado.pnt;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<Producto> productos = Arrays.asList(
+                new Producto("Coca-Cola Zero", "Litros", 1, 20),
+                new Producto("Coca-Cola", "Litros", 1, 18),
+                new Producto("Coca-Cola Zero", "Litros", 1, 20),
+                new Producto("Coca-Cola Zero", "Litros", 1, 20),
+
+
+
+
+
+
+
+
+    }
+}
